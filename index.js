bb06de5aab77d5df67805f0084bb7aff44b8747f
Mwagiru <joymwagiru@gmail.com>
2022-04-12 21:37:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+//Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    foo();
+});
+function foo(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
+
